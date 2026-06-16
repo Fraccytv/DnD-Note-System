5cf1c1e35b19a5e08ac1dfb42eff209f8ca0b7b0
Fraccy <Kennet3002@gmail.com>
2026-06-16 13:18:38 +0200

    Add campaign migration, views and templates
    Create initial migration for campaign and note models, register campaign in admin, and introduce class-based views and templates for creating campaigns. Changes include: added notes/migrations/0001_initial.py, registered campaign in notes/admin.py, replaced function-based view with MainView and MakeCampaignView in notes/views.py (including form handling and saves), updated notes/urls.py to use CBVs and added make_campaign route, added templates/make_campaign.html and templates/campaign.html, removed old templates/make_campaign file, and updated templates/home.html to link to the new make_campaign page. Debug prints are present in the new views for GET/POST flow.

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.mainview.home, name='home'),
+    path('', views.MainView.as_view(), name='home'),
+    path('make_campaign/', views.MakeCampaignView.as_view(), name='make_campaign'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import campaignForm
 
 # Create your views here.
 
-class mainview():
-    def home(request):
-        return render(request, 'home.html')
-    
-    def make_campaign(request):
-        return render(request, 'make_campaign.html')
+
+class MainView(View):
+    def get(self, request):
+        print("MainView GET")
+        return render(request, "home.html")
+
+
+class MakeCampaignView(View):
+    def get(self, request):
+        form = campaignForm()
+        return render(request, "make_campaign.html", {"form": form})
+
+    def post(self, request):
+        form = campaignForm(request.POST)
+        print("MakeCampaignView POST", form)
+        if form.is_valid():
+            print("Form is valid")
+            form.save()
+        else:
+            print("Form is not valid", form.errors)
+
+        return render(request, "home.html", {"form": form})
