d4d9fa4a9f6c65bf7183d79179f3f7376348a67c
Fraccy <Kennet3002@gmail.com>
2026-06-16 10:17:11 +0200

    init 2.0

--- dndnotes/settings.py
+++ dndnotes/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
-    'rest_framework',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
--- notes/models.py
+++ notes/models.py
@@ -5,8 +5,18 @@ from django.db import models
 class note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+class category(models.Model):
+    name = models.CharField(max_length=100)
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
